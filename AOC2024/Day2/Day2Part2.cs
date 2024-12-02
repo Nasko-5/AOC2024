bf8db62a47051e4bceb7a2698ccd7bb8f8941d54
Nasko-5 <sect0r.mainacc@gmail.com>
2024-12-02 12:12:30 +0200

    day 2 part 1 and day 2 part 2 partially

--- AOC2024/Day2/Day2Part2.cs
+++ AOC2024/Day2/Day2Part2.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,13 +13,78 @@ namespace AOC2024.Day2
     {
         public object Parse()
         {
-            return RawInput;
+            var reports = RawInput
+                .Replace("\r", "")
+                .Split('\n')
+                .Select(a => a.Split(' ').Select(int.Parse).ToList())
+                .ToArray();
+
+            return reports;
         }
 
         public void Solve()
         {
-            Console.WriteLine(Parse());
-            GotAnswer = 0;
+            List<int>[] reports = (List<int>[])Parse();
+            int safeReports = 0;
+
+            Console.WriteLine();
+            for (int reportIndex = 0; reportIndex < reports.Length; reportIndex++)
+            {
+                List<int> tempReport = reports[reportIndex];
+
+                Console.WriteLine($"Testing report {reportIndex} ({string.Join(" ", tempReport)})");
+                
+                for (int level = 0; level < tempReport.Count; level++)
+                {
+                    string levelString = $"{level}{(level == 1 ? "st" : level == 2 ? "nd" : level == 3 ? "rd" : "th")}";
+                    tempReport = new List<int>(reports[reportIndex]);
+                    tempReport.RemoveAt(level);
+                    Console.Write($"   Removing the {levelString} level -> ({string.Join(" ", tempReport)})");
+                    if (reportIsSafe(tempReport))
+                    {
+                        Console.WriteLine(" | Safe!");
+                        safeReports++;
+                        break;
+                    } else
+                    {
+                        Console.WriteLine(" | Not Safe.");
+                    }
+                    
+                }
+                //Console.ReadLine();
+            }
+
+            Console.WriteLine($"\nSafe reports: {safeReports}");
+
+            GotAnswer = safeReports;
+        }
+
+
+        private bool reportIsSafe(List<int> report)
+        {
+            int delta = 0;
+            int sign = 0;
+            int oldSign = Math.Sign(report[0] - report[1]);
+            bool isSafe = false;
+
+            for (int level = 0; level < report.Count - 1; level++)
+            {
+                delta = report[level] - report[level + 1];
+                sign = Math.Sign(delta);
+                delta = Math.Abs(delta);
+
+                //Console.Write($"   delta = {delta} | ");
+                if (delta < 1 || delta > 3 || sign != oldSign)
+                {
+                    isSafe = false;
+                    break;
+                }
+                isSafe = true;
+
+                oldSign = sign;
+            }
+
+            return isSafe;
         }
 
         public bool Solved { get { return CorrectAnswer == GotAnswer; } }

15b1fe4747495eedabbe70fd9f1f76dffd992ec8
Nasko-5 <sect0r.mainacc@gmail.com>
2024-12-08 00:22:26 +0200

    solution for day 5 part 1!!!!!!!!!!!

--- AOC20244/Day5/Day5Part1.cs
+++ AOC20244/Day5/Day5Part1.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Globalization;
+using System.Linq;
 
 namespace AOC2024.Day4
 {
@@ -7,16 +8,59 @@ namespace AOC2024.Day4
     {
         public object Parse()
         {
-            return 0;
+            var splitInput = RawInput
+                .Replace("\r", "")
+                .Split("\n\n");
+
+            int[][] rules = splitInput[0]
+                .Split('\n')
+                .Select(a => a.Split('|').Select(int.Parse).ToArray())
+                .ToArray();
+
+            int[][] updates = splitInput[1]
+                .Split('\n')
+                .Select(a => a.Split(',').Select(int.Parse).ToArray())
+                .ToArray();
+
+            (int[][] rules, int[][] pages) parsed = (rules, updates);
+            return parsed;
         }
 
         public void Solve()
         {
-            
+            (int[][] rules, int[][] updates) parsed = ((int[][] rules, int[][] updates))Parse();
+
+            var validUpdates = parsed.updates.Where(update => verifyUpdate(parsed.rules, update));
+
+
+            Console.WriteLine("\nValid Updates: \n" + string.Join("\n", validUpdates.Select(a => "   " + string.Join(" ", a))));
+
+            int midSum = validUpdates.Sum(a => a[(int)Math.Floor((double)a.Length / 2)]);
+
+            Console.WriteLine($"\nSum of middle numbers: {midSum}\n");
+        }
+
+        bool verifyUpdate(int[][] rules, int[] update)
+        {
+            var relevantRules = rules.Where(a => update.Contains(a[0]) && update.Contains(a[1])).ToArray();
+
+            foreach (int page in update)
+            {
+                var relevantRulesForUpdate = relevantRules.Where(a => a[0] == page);
+
+                foreach (var relevantRule in relevantRulesForUpdate)
+                {
+                    int xPos = Array.IndexOf(update, page);
+                    int yPos = Array.IndexOf(update, relevantRule[1]);
+
+                    if (xPos >= yPos) return false;
+                }
+            }
+            return true;
         }
 
         public bool Solved { get { return CorrectAnswer == GotAnswer; } }
-        public int CorrectAnswer => 43;
+        public int CorrectAnswer => 7198;
         public int GotAnswer { get; set; }
         public bool Debug { get; set; }
         public string Path { get; set; }
